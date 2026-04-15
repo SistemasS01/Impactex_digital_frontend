9116d33bca19e28f91e58de540310d80d3a549b5
SistemasS01 <sistemas@corporacionimpactex.com>
2026-04-15 16:34:05 -0500

    Se arreglo las variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
+const apiUrl =
+  typeof process !== 'undefined' && process.env?.['BITE_API_URL']
+    ? process.env['BITE_API_URL']
+    : 'https://api.corporacionimpactex.com';
+
 export const environment = {
   production: true,
-  apiUrl: 'https://api.corporacionimpactex.com'
+  apiUrl,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
+const apiUrl =
+  typeof process !== 'undefined' && process.env?.['BITE_API_URL']
+    ? process.env['BITE_API_URL']
+    : 'http://localhost:5118';
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5118'
+  apiUrl,
 };
