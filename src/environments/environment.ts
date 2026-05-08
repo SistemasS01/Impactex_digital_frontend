8e7f0a2d73cb2b68984017489c0345b1e5d22626
El Jonas <jona16lozada@gmail.com>
2026-05-08 16:45:56 -0500

    Se arreglo la conexion del front con  el back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
-const apiUrl =
-  typeof process !== 'undefined' && process.env?.['BITE_API_URL']
-    ? process.env['BITE_API_URL']
-    : '';
+const rawApiUrl =
+  (typeof process !== 'undefined' && process.env?.['BITE_API_URL']) ||
+  (typeof globalThis !== 'undefined' && (globalThis as any).BITE_API_URL);
+
+const apiUrl = rawApiUrl.replace(/\/+$/, '');
 
 export const environment = {
   production: false,
