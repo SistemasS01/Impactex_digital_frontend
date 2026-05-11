bf6a7c0a320aaaf8610b32c0ec4b608b0c67cb2a
El Jonas <jona16lozada@gmail.com>
2026-05-11 15:47:31 -0500

    fix: hardcode backend URL in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://impactex-web-api.runasp.net',
+  apiUrl: process.env?.['BITE_API_URL'] || (globalThis as any).BITE_API_URL || '',
 };
